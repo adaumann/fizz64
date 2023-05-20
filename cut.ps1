054b98d0c6b9cf7f7b3049ac4a26d58e082c0574
mail@adaumann.de <mail@adaumann.de>
2023-05-20 07:22:42 +0200

    loading works with cut

---
+++ cut.ps1
@@ -0,0 +1,6 @@
+  $stream = [System.IO.File]::OpenRead("game.prg")
+  $barr = New-Object byte[] 65535
+  $bytesRead = $stream.Read($barr,0, 50944)
+  $ostream = [System.IO.File]::OpenWrite("game_s.prg")
+  $ostream.Write($barr,2,$bytesRead);
+  $ostream.close();
