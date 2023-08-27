c81eb41a944ea78045818756b36c3cce15a02de4
mail@adaumann.de <mail@adaumann.de>
2023-08-27 07:27:50 +0200

    levcomplete

--- cut.ps1
+++ cut.ps1
@@ -13,28 +13,3 @@
   $ostream.Write($barr,2,$bytesRead);
   $ostream.close();
   $stream.close();
-
-  $stream = [System.IO.File]::OpenRead("sounds/s.prg")
-  $barr = New-Object byte[] 65535
-  $bytesRead = $stream.Read($barr,0, 839)
-  $ostream = [System.IO.File]::OpenWrite("sounds/_s.prg")
-  $ostream.Write($barr,2,$bytesRead);
-  $ostream.close();
-  $stream.close();
-
-  $stream = [System.IO.File]::OpenRead("sounds/a1.prg")
-  $barr = New-Object byte[] 65535
-  $bytesRead = $stream.Read($barr,0, 1026)
-  $ostream = [System.IO.File]::OpenWrite("sounds/_a1.prg")
-  $ostream.Write($barr,2,$bytesRead);
-  $ostream.close();
-  $stream.close();
-
-
-  # $binaryData = [Byte[]] (0x00, 0xE0)
-  # [IO.File]::WriteAllBytes("./music/fizz_ingame_e000.bin", $binaryData)
-  # cmd /c copy /b music\fizz_ingame_e000.bin+music\_fizz_ingame_e000.dat music\fizz_ingame_e000.bin 
-
-  # $binaryData = [Byte[]] (0x00, 0x20)
-  # [IO.File]::WriteAllBytes("./music/fizz_intro_2000.bin", $binaryData)
-  # cmd /c copy /b music\fizz_intro_2000.bin+music\_fizz_intro_2000.dat music\fizz_intro_2000.bin 
